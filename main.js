a7f85671fca86c438ace7246bab8e66da9476657
Michal Nowak <michalnowak.contact@gmail.com>
2016-01-17 18:02:13 +0100

    another change

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 function add (adding) {
 	var li = document.createElement("li")
 	var ul = document.getElementsByTagName("ul")[0];
-	li.innerText="Developer";
+	li.innerText = "Developer";
 	ul.appendChild(li);
 }
 
 add("li");
+
+/*function addNew (adding2) {
+	var ul = document.getElementsByTagName("ul")[0] += ul.innerText = "<li>Developer</li>"
+	ul.appendChild(li);
+}
+
+addNew("li");*/
